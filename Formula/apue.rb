ef79aded280961e7bd52fe9fcf406beb7d9914dd
chensheng06 <chensheng06@baidu.com>
2019-10-10 11:50:23 +0800

    test own repo

--- Formula/apue.rb
+++ Formula/apue.rb
@@ -1,16 +1,20 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook.html
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
- class Apue < Formula
-  desc "libapue for apue\n    ----by saka"
-  homepage "https://juejin.im/user/5855ebea8d6d810065a4befa"
-  url "https://raw.githubusercontent.com/rangaofei/apue/master/lib/apue-1.0.tar.gz"
+class Apue < Formula
+  desc ""
+  homepage ""
+  url "https://bie-test.gz.bcebos.com/apue-1.0.tar.gz"
   sha256 "7e84d03563f7f0119f2d946cc9f439192e582b65a504c39f4158fea7f38f7cbd"
-   def install
-     lib.install "libapue.a"
-     include.install "apue.h"
-   end
-   test do
+
+  # depends_on "cmake" => :build
+
+  def install
+    lib.install "libapue.a"
+    include.install "apue.h" 
+  end
+
+  test do
     # `test do` will create, run in and delete a temporary directory.
     #
     # This test will fail and we won't accept that! For Homebrew/homebrew-core
